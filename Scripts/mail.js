d6fb849ad962267e20796540a35c3d057e132e43
Victor Iovanescu <iovanescu.victor@yahoo.com>
2019-12-10 13:58:33 +0200

    add mail box

---
+++ Scripts/mail.js
@@ -0,0 +1,11 @@
+// Email.send({
+//     Host : "smtp.elasticemail.com",
+//     Username : "iovanescu.victor@yahoo.com",
+//     Password : "04d80d66-87c7-4daf-86a4-fb3d571043c5",
+//     To : 'iovanescu.victor55@gmail.com',
+//     From : "iovanescu.victor55@gmail.com",
+//     Subject : "Test",
+//     Body : "Test"
+// }).then(
+//   message => console.log(message)
+// );  
--- Scripts/slideshow.js
+++ Scripts/slideshow.js
@@ -1,4 +1,24 @@
-console.log('start');
 
-let currentImageIndex = 0;
-let imagesArray;
+const numberOfImages = 12;
+let currentImageIndex = 1;
+let path ="../Assets/HobbyImages/";
+
+function changeImage(index) {
+    document.querySelector(".center-image img").src= path + index + ".jpg";
+}
+
+function changeImageForward() {
+    if (currentImageIndex + 1 <= numberOfImages) 
+        currentImageIndex++;
+        else currentImageIndex = 1;
+    changeImage(currentImageIndex);
+}
+
+function changeImageBackward() {
+    if (currentImageIndex - 1 != 0) 
+        currentImageIndex--;
+        else currentImageIndex = numberOfImages ;
+    changeImage(currentImageIndex);
+}
+
+changeImage(currentImageIndex);
